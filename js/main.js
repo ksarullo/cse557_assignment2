1d5c68015335b2fb37c2b81c4ab9a16f969d02bc
Saulet Yskak <sauletik01@gmail.com>
2020-02-09 16:50:05 -0600

    Display route for 1 employee with ID 10

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-console.log("main.js loaded")
+// console.log("main.js loaded")
 
 var width = document.getElementById('map-card').offsetWidth - 100;
 var height = document.getElementById('map-card').offsetHeight - 100;
@@ -6,30 +6,77 @@ var height = document.getElementById('map-card').offsetHeight - 100;
 var projection = d3.geoMercator()
 var path = d3.geoPath().projection(projection);
 
-
 var svg = d3.select('#map-card').append('svg')
-            .attr("preserveAspectRatio", "xMinYMin meet")
-            .attr("viewBox", "0 0 " + String(width) + " " + String(height))
-            // Class to make it responsive.
-            .classed("svg-content-responsive", true)
+    .attr("preserveAspectRatio", "xMinYMin meet")
+    .attr("viewBox", "0 0 " + String(width) + " " + String(height))
+    // Class to make it responsive.
+    .classed("svg-content-responsive", true)
 var zoom = d3.zoom()
-                .scaleExtent([1, 40])
-                .translateExtent([[-100, -100], [width + 90, height + 100]])
-                .on("zoom", zoomed);
+    .scaleExtent([1, 40])
+    .translateExtent([
+        [-100, -100],
+        [width + 90, height + 100]
+    ])
+    .on("zoom", zoomed);
 
+d3.queue()
+    .defer(d3.json, 'data/Abila.json')
+    .defer(d3.csv, 'data/gps_1.csv') //
+    // .defer(d3.csv, 'data/gps.csv')
+    // .defer(d3.csv, 'data/car-assignments.csv')
+    .await(ready);
 
-d3.json('data/Abila.json', function(error, d) {
-
-    console.log("geojson", d.features)
-    projection.fitExtent([[0, 0], [width, height]], d);
+function ready(error, d, gpsWithID10) {
+    projection.fitExtent([
+        [0, 0],
+        [width, height]
+    ], d);
 
     view = svg.selectAll("path")
         .data(d.features)
         .enter().append('path')
         .attr('d', path)
 
+    drawRoutes(gpsWithID10)
 
-});
+}
+
+function drawRoutes(data) {
+    // Convert String to Number
+    data = data.map(d => {
+        return {
+            timestamp: d.Timestamp,
+            id: +d.id,
+            lat: +d.lat,
+            lon: +d.long
+        }
+    })
+
+    var links = [];
+
+    // Generate Links
+    for (var i = 0, len = data.length - 1; i < len; i++) {
+        links.push({
+            type: "LineString",
+            coordinates: [
+                [data[i].lon, data[i].lat],
+                [data[i + 1].lon, data[i + 1].lat]
+            ]
+        });
+    }
+
+    // Add the path
+    svg.selectAll("myPath")
+        .data(links)
+        .enter()
+        .append("path")
+        .attr("d", function (d) {
+            return path(d)
+        })
+        .style("fill", "none")
+        .style("stroke", "#69b3a2")
+        .style("stroke-width", 3)
+}
 
 
 //TODO: Add a zoom control to at least tell the user that zooming is available.
@@ -39,10 +86,10 @@ svg.call(zoom);
 
 function zoomed() {
     view.attr("transform", d3.event.transform);
-  }
-  
+}
+
 function resetted() {
     svg.transition()
         .duration(750)
         .call(zoom.transform, d3.zoomIdentity);
-  }
+}
